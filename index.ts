5b28f4bd8c3c11e3ea0a9fe656621f56d3d1dc12
Hong jinho <tpdleps@gmail.com>
2018-05-26 21:13:46 +0900

    init block example

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("test")
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+  }
+}
+
+const genesisBlock:Block= new Block(0,"123456","","hello",123456)
+
+let blockchain: [Block] = [genesisBlock];
+
+console.log(blockchain)
